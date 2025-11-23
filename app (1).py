f7c076be735535adf8ce4196510d91c44005fbac
khitamahmad302-sketch <khitamahmad302@gmail.com>
2025-11-23 20:52:05 +0300

    Add files via upload

--- app (1).py
+++ app (1).py
@@ -8,7 +8,9 @@ import checkout
 st.set_page_config(page_title="Marble Baker", layout="wide")
 
 
+
 logo = Image.open("لوجو.png")
+
 
 marble_baker = Bakery()
 
@@ -19,22 +21,20 @@ if "cart" not in st.session_state:
         "Mojitos": [0]*len(marble_baker.mojitos)
     }
 
+
 st.sidebar.title("Marble Baker Menu 🍰☕🍹")
 menu_option = st.sidebar.radio(
     "Go to:", ["Home 🏨", "Sweets 🍰", "Coffee ☕", "Mojitos 🍹", "Checkout 🛒"]
 )
 
+
 if menu_option == "Home 🏨":
     home.show_home(logo)
-
 elif menu_option == "Sweets 🍰":
     menu.show_items_box("Sweets", marble_baker)
-
 elif menu_option == "Coffee ☕":
     menu.show_items_box("Coffee", marble_baker)
-
 elif menu_option == "Mojitos 🍹":
     menu.show_items_box("Mojitos", marble_baker)
-
 elif menu_option == "Checkout 🛒":
     checkout.checkout_page(marble_baker)
